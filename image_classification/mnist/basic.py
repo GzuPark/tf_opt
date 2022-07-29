1cae17f8cadfe9400014b6305199d4b081acae4b
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-07-29 19:57:56 +0900

    Split BasicModel

---
+++ image_classification/mnist/basic.py
@@ -0,0 +1,83 @@
+import logging
+import os
+
+from time import perf_counter
+from typing import Any, Dict
+
+import tensorflow as tf
+
+from image_classification.mnist import BaseModel
+
+
+class BasicModel(BaseModel):
+
+    def __init__(
+            self,
+            root_dir: str,
+            dataset: Dict[str, Any],
+            valid_split: float,
+            batch_size: int,
+            epochs: int,
+            logger: logging.Logger,
+            verbose: bool = False,
+    ) -> None:
+        super().__init__(root_dir, dataset, valid_split, batch_size, epochs)
+
+        self.model = None
+        self.model_path = os.path.join(self.ckpt_dir, "mnist_none_keras.h5")
+        self.verbose = 1 if verbose else 0
+
+        self._logger = logger
+        self._logger.info("Run without optimizing")
+
+    def create_model(self) -> None:
+        input_layer = tf.keras.Input(shape=(28, 28))
+        x = tf.keras.layers.Reshape(target_shape=(28, 28, 1))(input_layer)
+        x = tf.keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation="relu")(x)
+        x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+        x = tf.keras.layers.Flatten()(x)
+        outputs = tf.keras.layers.Dense(10)(x)
+
+        self.model = tf.keras.Model(inputs=input_layer, outputs=outputs)
+
+        if self.verbose:
+            self.model.summary()
+
+    def _compile(self) -> None:
+        self.model.compile(
+            optimizer="adam",
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=["accuracy"],
+        )
+
+    def train(self) -> None:
+        if os.path.exists(self.model_path):
+            self.model = tf.keras.models.load_model(self.model_path)
+            return
+
+        train_kwargs = dict()
+        train_kwargs["batch_size"] = self.batch_size
+        train_kwargs["epochs"] = self.epochs
+        train_kwargs["validation_split"] = self.valid_split
+        train_kwargs["verbose"] = self.verbose
+
+        self._compile()
+        self.model.fit(self.x_train, self.y_train, **train_kwargs)
+
+        tf.keras.models.save_model(self.model, self.model_path, include_optimizer=True)
+
+    def evaluate(self) -> Dict[str, Any]:
+        start_time = perf_counter()
+        _, accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=self.verbose)
+        end_time = perf_counter()
+
+        result = dict()
+        result["method"] = "keras"
+        result["opt"] = "none"
+        result["accuracy"] = accuracy
+        result["total_time"] = end_time - start_time
+        result["model_file_size"] = os.path.getsize(self.model_path)
+
+        self._logger.info(result)
+
+        return result
