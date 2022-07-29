afe242a0eff59d0b4dca297118351508190a095f
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-07-29 19:57:48 +0900

    Split BaseModel

---
+++ image_classification/mnist/base.py
@@ -0,0 +1,39 @@
+import os
+
+from typing import Any, Dict
+
+
+class BaseModel(object):
+
+    def __init__(
+            self,
+            root_dir: str,
+            dataset: Dict[str, Any],
+            valid_split: float = 0.0,
+            batch_size: int = 128,
+            epochs: int = 5,
+    ) -> None:
+        self.valid_split = valid_split
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+        self.x_train = dataset["x_train"]
+        self.y_train = dataset["y_train"]
+        self.x_test = dataset["x_test"]
+        self.y_test = dataset["y_test"]
+
+        self.ckpt_dir = os.path.join(root_dir, "ckpt")
+        if not os.path.exists(self.ckpt_dir):
+            os.makedirs(self.ckpt_dir)
+
+    def _compile(self) -> None:
+        raise NotImplementedError
+
+    def create_model(self) -> None:
+        raise NotImplementedError
+
+    def train(self) -> None:
+        raise NotImplementedError
+
+    def evaluate(self) -> Dict[str, Any]:
+        raise NotImplementedError
